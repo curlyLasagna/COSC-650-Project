87ab6c759af959c6149c024bfc4d6990507a8735
luis <luis.gcodes@gmail.com>
2025-07-24 22:31:46 -0400

    dos2unix because why

--- NetServerV2.java
+++ NetServerV2.java
@@ -4,37 +4,41 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class NetServerV2 {
-    public static void main(String[] args) {
-		Scanner myScanner = new Scanner(System.in);  // Create a Scanner object
+public class NetServerV2 extends Thread {
+
+	public class ClientListener {
+
+	}
+
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in); // Create a Scanner object
 		System.out.println("Enter the IP address of the server, Enter for loopback");
 		String host = myScanner.nextLine();
 		if (host.isEmpty()) {
 			host = "127.0.0.1";
 		}
-		
-		int port = 42181;
-		System.out.println("Enter the port number that the server is listening on, Enter for 42181");
-		String sPort = myScanner.nextLine();
-		if (!sPort.isEmpty()) {
-			port = Integer.parseInt(sPort);
+
+		// Default port of 11122
+		int port = 11122;
+		System.out.println("Enter timeout period");
+		if (myScanner.nextLine().isEmpty()) {
 		}
 
-        try {
-            ServerSocket serverSocket = new ServerSocket();
-            serverSocket.bind(new InetSocketAddress(host, port));
-            System.out.println("Server: Listening on " + host + ":" + port);
+		try {
+			ServerSocket serverSocket = new ServerSocket();
+			serverSocket.bind(new InetSocketAddress(host, port));
+			System.out.println("Server: Listening on " + host + ":" + port);
 
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Connected from: " + clientSocket.getRemoteSocketAddress());
-                clientSocket.getOutputStream().write("Hello Client".getBytes());
-                clientSocket.close();
+			while (true) {
+				Socket clientSocket = serverSocket.accept();
+				System.out.println("Connected from: " + clientSocket.getRemoteSocketAddress());
+				clientSocket.getOutputStream().write("Hello Client".getBytes());
+				clientSocket.close();
 				System.exit(0);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		myScanner.close();
+	}
 }
-
